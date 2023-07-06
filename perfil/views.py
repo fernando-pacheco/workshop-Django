4d6b1358b3ed1d03b8c4be696f1a26f547de3882
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2023-07-05 21:16:16 -0300

    criando o gerenciamento da conta e cadastro

--- perfil/views.py
+++ perfil/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
     return render(request,'home.html')
+
+def gerenciar(request):
+    return render(request,'gerenciar.html')
+
+def cadastrar_banco(request):
+    return render(request,'cadastrar_banco.html')
+
