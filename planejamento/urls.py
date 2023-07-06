939c0fe482356553bdc99bbd8012822bcac3b397
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2023-07-06 16:44:39 -0300

    planejamento definido

--- planejamento/urls.py
+++ planejamento/urls.py
@@ -5,4 +5,5 @@ from perfil.models import Conta, Categoria
 
 urlpatterns = [
     path('definir_planejamento/', views.definir_planejamento, name="definir_planejamento"),
+    path('update_valor_categoria/<int:id>', views.update_valor_categoria, name="update_valor_categoria"),
 ]
